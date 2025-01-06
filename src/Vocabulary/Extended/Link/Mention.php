5aad40c50459270b4e192bc46ae28467fb061e9f
Dominik Chrastecky <dominik@chrastecky.cz>
2025-01-06 21:58:42 +0100

    Document mention

--- src/Vocabulary/Extended/Link/Mention.php
+++ src/Vocabulary/Extended/Link/Mention.php
@@ -4,6 +4,9 @@ namespace Rikudou\ActivityPub\Vocabulary\Extended\Link;
 
 use Rikudou\ActivityPub\Vocabulary\Core\Link;
 
+/**
+ * A specialized {@see Link} that represents an @mention.
+ */
 class Mention extends Link
 {
     public string $type {
