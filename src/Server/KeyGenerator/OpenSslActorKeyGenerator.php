d29befd8c8a07326bb0300efe91749c32de84e47
Dominik Chrastecky <dominik@chrastecky.cz>
2025-02-06 22:39:03 +0100

    Use lazy ghost for generating keys

--- src/Server/KeyGenerator/OpenSslActorKeyGenerator.php
+++ src/Server/KeyGenerator/OpenSslActorKeyGenerator.php
@@ -2,31 +2,41 @@
 
 namespace Rikudou\ActivityPub\Server\KeyGenerator;
 
+use ReflectionClass;
+use ReflectionObject;
 use Rikudou\ActivityPub\Dto\KeyPair;
 use Rikudou\ActivityPub\Dto\PublicKey;
 use Rikudou\ActivityPub\Exception\CryptographyException;
-use Rikudou\ActivityPub\Server\KeyGenerator\ActorKeyGenerator;
 use Rikudou\ActivityPub\Vocabulary\Contract\ActivityPubActor;
 
 final readonly class OpenSslActorKeyGenerator implements ActorKeyGenerator
 {
     public function generate(?ActivityPubActor $actor = null, int $bits = 4096): KeyPair
     {
-        $config = [
-            'private_key_type' => OPENSSL_KEYTYPE_RSA,
-            'private_key_bits' => $bits,
-        ];
-        $resource = openssl_pkey_new($config) ?: throw new CryptographyException('Failed generating new private key');
+        $reflection = new ReflectionClass(KeyPair::class);
+        $keyPair = $reflection->newLazyGhost(function (KeyPair $keyPair) use ($actor, $bits) {
+            $config = [
+                'private_key_type' => OPENSSL_KEYTYPE_RSA,
+                'private_key_bits' => $bits,
+            ];
+            $resource = openssl_pkey_new($config) ?: throw new CryptographyException('Failed generating new private key');
 
-        $privateKeyPem = '';
-        openssl_pkey_export($resource, $privateKeyPem);
-        assert(is_string($privateKeyPem));
+            $privateKeyPem = '';
+            openssl_pkey_export($resource, $privateKeyPem);
+            assert(is_string($privateKeyPem));
 
-        $details = openssl_pkey_get_details($resource) ?: throw new CryptographyException('Failed decoding the private key');
-        $publicKeyPem = $details['key'];
-        assert(is_string($publicKeyPem));
+            $details = openssl_pkey_get_details($resource) ?: throw new CryptographyException('Failed decoding the private key');
+            $publicKeyPem = $details['key'];
+            assert(is_string($publicKeyPem));
 
-        $keyPair = new KeyPair($privateKeyPem, $publicKeyPem);
+            $reflection = new ReflectionObject($keyPair);
+            $reflection->getProperty('privateKey')->setValue($privateKeyPem);
+            $reflection->getProperty('publicKey')->setValue($publicKeyPem);
+
+            return $keyPair;
+        });
+        assert($keyPair instanceof KeyPair);
+
         if ($actor) {
             $actor->publicKey = new PublicKey(
                 id: $actor->id . '#mainKey',
